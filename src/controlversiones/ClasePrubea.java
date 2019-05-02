d9b4dafe71ab741a01b38343ac7a53026f6bd6f2
Usuario1 <Usuario1@AlexanderConteron>
2019-05-02 12:36:49 -0500

    francis1213

--- src/controlversiones/ClasePrubea.java
+++ src/controlversiones/ClasePrubea.java
@@ -12,5 +12,6 @@ package controlversiones;
 public class ClasePrubea {
     public static void main(String[] args) {
         System.out.println("FRANCIS PUTO");
+        System.out.println("FRANCIS PUTO2");
     }
 }
