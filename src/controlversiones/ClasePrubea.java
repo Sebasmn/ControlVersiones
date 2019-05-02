d047283ddc8a34cdcee27562bc9a2367a7a67b62
efejota <efejota@Javier>
2019-05-02 12:39:13 -0500

    NUEVIUTO

--- src/controlversiones/ClasePrubea.java
+++ src/controlversiones/ClasePrubea.java
@@ -12,6 +12,6 @@ package controlversiones;
 public class ClasePrubea {
     public static void main(String[] args) {
         System.out.println("FRANCIS PUTO");
-        System.out.println("FRANCIS PUTO2");
+        System.out.println("FRANCIS no esPUTO2");
     }
 }
