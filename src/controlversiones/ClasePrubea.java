25f47d06e314da22eace85601d3c1ef8fcd0de9d
efejota <efejota@Javier>
2019-05-02 13:23:12 -0500

    J

--- src/controlversiones/ClasePrubea.java
+++ src/controlversiones/ClasePrubea.java
@@ -11,8 +11,7 @@ package controlversiones;
  */
 public class ClasePrubea {
     public static void main(String[] args) {
-        System.out.println("FRANCIS PUTO");
-        System.out.println("FRANCIS no esPUTO2");
+
         System.out.println("QUIETO PUTO NO ME ESTES PICOTEANDO! ");
         //COMENTARIO!
         System.out.println("jol");
