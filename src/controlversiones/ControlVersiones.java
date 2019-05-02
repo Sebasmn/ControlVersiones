4a5eca0ec4fd713447027999e0e5d87ae4fb8274
ESTUDIANTE <ESTUDIANTE@CTT-PC05>
2019-05-02 08:26:43 -0500

    cambio2

--- src/controlversiones/ControlVersiones.java
+++ src/controlversiones/ControlVersiones.java
@@ -17,6 +17,7 @@ public class ControlVersiones {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("CAMBIO1");
+        System.out.println("Cambio2");
     }
     
 }
