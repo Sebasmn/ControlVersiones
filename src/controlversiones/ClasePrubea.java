c7a2485a1bdbe2221a9131c3292cda5a56edcdba
Usuario1 <Usuario1@AlexanderConteron>
2019-05-02 12:46:37 -0500

    sb4

--- src/controlversiones/ClasePrubea.java
+++ src/controlversiones/ClasePrubea.java
@@ -13,6 +13,5 @@ public class ClasePrubea {
     public static void main(String[] args) {
         System.out.println("FRANCIS PUTO");
         System.out.println("FRANCIS no esPUTO2");
-        //System.out.println("FRANCIS no esPUTO3");
     }
 }
