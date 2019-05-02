2c482e0c09411802cf0f9836292fb883b6dc4000
ESTUDIANTE <ESTUDIANTE@CTT-PC05>
2019-05-02 12:50:45 -0500

    pruebis

--- src/controlversiones/ControlVersiones.java
+++ src/controlversiones/ControlVersiones.java
@@ -32,6 +32,7 @@ public class ControlVersiones {
         System.out.println("dfgdsgdgfdsf");
         System.out.println("INGENIERIA");
         System.out.println("SOY JAVIER AMIGOS");
+        System.out.println("Op");
     }
     
 }
